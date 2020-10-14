b47ce8826ddda607c70ecd63965a437dbdbdb3f1
vinodh1988 <k.c.vinodh@hotmail.com>
2020-10-14 15:12:04 +0530

    firstcommit

---
+++ Entities/Person.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Entities
+{
+    public class Person
+    {
+        public int sno { get; set; }
+        public string name { get; set; }
+
+        public string city { get; set; }
+    }
+}
